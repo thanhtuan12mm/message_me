b994581b8ca8e3b15f404247559bb62e4b9177fa
thanhtuan12mm <thanhtuan12mm@gmail.com>
2021-08-24 17:40:41 +0700

    Build user resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "thanhtuan2", password: "camtien")
+User.create(username: "thanhtuan3", password: "camtien")
+User.create(username: "thanhtuan4", password: "camtien")
+User.create(username: "thanhtuan5", password: "camtien")
+User.create(username: "thanhtuan6", password: "camtien")
